48bb59b723340452cf20a9fdedbfa1e53a6b6896
kramsg12 <33532860+kramsg12@users.noreply.github.com>
2020-09-06 20:31:31 +1000

    Update Deploy_arch.sh

--- Deploy_arch.sh
+++ Deploy_arch.sh
@@ -32,17 +32,20 @@ exec 2> >(tee "stderr.log")
 timedatectl set-ntp true
 
 ### Setup the disk and partitions ###
+swap_size=$(free --mebi | awk '/Mem:/ {print $2}')
+swap_end=$(( $swap_size + 129 + 1 ))MiB
 
 parted --script "${device}" -- mklabel gpt \
   mkpart ESP fat32 1Mib 129MiB \
   set 1 boot on \
-  mkpart primary ext4 100%
+  mkpart primary linux-swap 129MiB ${swap_end} \
+  mkpart primary ext4 ${swap_end} 100%
 
 # Simple globbing was not enough as on one device I needed to match /dev/mmcblk0p1
 # but not /dev/mmcblk0boot1 while being able to match /dev/sda1 on other devices.
 part_boot="$(ls ${device}* | grep -E "^${device}p?1$")"
 part_swap="$(ls ${device}* | grep -E "^${device}p?2$")"
-part_root="$(ls ${devchpasswdice}* | grep -E "^${device}p?3$")"
+part_root="$(ls ${device}* | grep -E "^${device}p?3$")"
 
 wipefs "${part_boot}"
 wipefs "${part_swap}"
@@ -60,7 +63,6 @@ mount "${part_boot}" /mnt/boot
 ### Install and configure the basic system ###
 wget https://raw.githubusercontent.com/kramsg12/kramsg1_repo/master/pacman.conf 
 mv -f pacman.conf /etc/pacman.conf
-pacman -Sy
 
 pacstrap /mnt kramsg1-base
 genfstab -t PARTUUID /mnt >> /mnt/etc/fstab
