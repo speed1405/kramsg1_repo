6031b916279f068950038047f0c0977a0ee06aa8
speed1405 <36902729+speed1405@users.noreply.github.com>
2020-09-07 10:25:14 +1000

    Update Deploy_arch.sh

--- Deploy_arch.sh
+++ Deploy_arch.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#WARNING the following packages are needed if installing on archiso:
+#bash, curl, wget, dialog as script dependants.
 # WARNING: this script will destroy data on the selected disk.
 # This script can be run by executing the following:
 #   curl -sL http://bit.ly/kramsg1arch | bash
